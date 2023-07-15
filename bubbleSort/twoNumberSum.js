3c5088adb90e131bc59a8d6f1292654443bb4a2a
umraooo7 <aku.kiet@gmail.com>
2023-07-15 15:20:56 +0530

    refining solution

--- bubbleSort/twoNumberSum.js
+++ bubbleSort/twoNumberSum.js
@@ -1,12 +1,19 @@
+// solution 1
+
 function twoNumberSum(array, targetSum) {
-  // Write your code here.
+  if (array.length <= 0) {
+    return array;
+  }
   for (let i = 0; i < array.length; i++) {
-    for (let j = i + 1; j < array.legth; j++) {
+    for (let j = i + 1; j < array.length; j++) {
       if (array[i] + array[j] === targetSum) {
         return [array[i], array[j]];
       }
     }
   }
+  return [];
 }
 
 console.log(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10));
+
+// solution 2
